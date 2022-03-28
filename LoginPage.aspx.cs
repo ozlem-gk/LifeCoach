28e9c39460f3591e82049024b0e0e880bbdf2b9e
ozlem goksu <ozlemgoksu4@gmail.com>
2022-03-28 20:17:07 +0300

    Add project files.

---
+++ LoginPage.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LifeCoach
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        TextBox textBoxmail;
+        TextBox textBoxpass;
+        Button btn;
+        DropDownList drp;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox textBoxmail =(TextBox) exampleInputEmail;
+            TextBox textBoxpass = (TextBox) exampleInputPassword;
+            Button btn = (Button) ButonGiris;
+            DropDownList drp = DropDownList1;
+
+
+
+
+        }
+
+        protected void ButonGiris_Click(object sender, EventArgs e)
+        {
+            
+            if (DropDownList1.SelectedValue == "Kullanıcı")
+            {
+                Response.Redirect("/Default.aspx");
+            }
+            else if(DropDownList1.SelectedValue == "Diyetisyen")
+            {
+                Response.Redirect("/DietitianPage.aspx");
+            }
+
+        }
+    }
+}
